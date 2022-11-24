503bc825ebd9230f54eb4b84e5e617c2d9be4467
Tommaso Torda <tommaso.torda@gmail.com>
2022-11-24 16:02:56 +0000

    Update Gradients.py

--- Gradients.py
+++ Gradients.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
@@ -18,17 +12,9 @@ import time
 import datetime
 import json
 
-
-# In[2]:
-
-
 from keras.models import load_model
 from keras.utils.vis_utils import plot_model
 from sklearn import preprocessing
-
-
-# In[11]:
-
 
 def dice0(y_true, y_pred, smooth = 1e-7):
     y_true_f = tf.reshape(tf.cast(y_true[:,:,:,0], 'float32'), [len(y_true), -1]) 
@@ -53,46 +39,28 @@ def dice3(y_true, y_pred, smooth = 1e-7):
     return (2*tf.reduce_sum(tf.abs(y_true_f*y_pred_f), axis = 1))/(tf.reduce_sum(
         y_true_f**2 + y_pred_f**2, axis = 1)+smooth)
 
-def dice_loss1(y_true, y_pred):
+def dice_loss(y_true, y_pred):
     a0 = 0
     a1 = 1
     a2 = 1
     a3 = 1
     return 1-(a0*dice0(y_true,y_pred)+a1*dice1(y_true,y_pred)+a2*dice2(
         y_true,y_pred)+a3*dice3(y_true,y_pred))/(a0+a1+a2+a3)
-
-
-class dice_loss(tf.keras.losses.Loss):
-    def __init__(self, reduction=keras.losses.Reduction.NONE, name='dice_loss'):
-        super().__init__(name=name, reduction=reduction)
         
-    def get_config(self):
-        config = super(dice_loss, self).get_config()
-        return config
-    
-    def call(self, y_true, y_pred):
-        
-        return dice_loss1(y_true,y_pred)
-        
-        
-
-
-# In[12]:
-
+#This is an example of calculation of gradients for TracIn application. In this case we choose to evaluate the
+#gradient of the last layer of the model on the training set considering only 10 central epochs of the training process. 
+#The loss function is the complete dice (the weigthed average of all the label), but we can choose different setup
+#for example we can focus only on a specific label choosing the correct "loss".
 
 checkpoint_dir = '/home/tordatom/Dati_Imaging/BraTs_19/checkpoints/training_checkpoints_Unet'
-model = tf.keras.models.load_model(checkpoint_dir+"/ckpt_Unet_01", custom_objects={'dice0': dice0, 'dice1': dice1, 'dice2': dice2, 'dice3': dice3, "dice_loss1":dice_loss1})
-
-
-# In[7]:
+model = tf.keras.models.load_model(checkpoint_dir+"/ckpt_Unet_01", custom_objects={'dice0': dice0, 'dice1': dice1, 'dice2': dice2, 'dice3': dice3, "dice_loss":dice_loss})
 
 
 file_list_ckpt = glob(os.path.join(checkpoint_dir, "*"))
 file_list_ckpt.sort()
 
 
-# In[3]:
-
+#when we load the example we want to store the name of the example in order to reconstruct to which example the specific #gradient belongs
 
 def load_image_train(image_file):
     data = np.load(image_file)
@@ -113,24 +81,13 @@ def test_reshape(index, X,Y):
     return index,X,Y
 
 
-# In[4]:
-
-
-search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/DataTracin_test/"
+search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/DataTracin_train/"
 os.chdir(search_dir)
 file_list_train = filter(os.path.isfile, os.listdir(search_dir))
 file_list_train = [os.path.join(search_dir, f) for f in file_list_train] # add path to each file
 file_list_train.sort(key=lambda x: os.path.getmtime(x))
 
-
-# In[5]:
-
-
 BATCH_SIZE = 1
-
-
-# In[7]:
-
 
 train_dataset = tf.data.Dataset.list_files(file_list_train, shuffle = False)
 train_dataset = train_dataset.map(lambda item: tf.numpy_function(
@@ -140,27 +97,17 @@ train_dataset = train_dataset.map(test_reshape)
 train_dataset = train_dataset.batch(BATCH_SIZE)
 
 
-# In[12]:
-
-
 def get_grad(X,Y, model):
 
     with tf.GradientTape() as tape:
-        loss = dice_loss1(model(X, training = False) , Y)
+        loss = dice_loss(model(X, training = False) , Y)
     grad = tape.gradient(loss, model.trainable_variables[80])
     grad = np.array(grad, dtype = "float32").reshape([-1])
 
     return grad
 
 
-# In[18]:
-
-
-os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/test_totdice")
-
-
-# In[37]:
-
+os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/train_totdice")
 
 grad_train = np.zeros((len(file_list_ckpt[2:12]), 256), dtype = "float32")
 
@@ -172,11 +119,11 @@ for k,h in enumerate(train_dataset):
                                                                   'dice1': dice1,
                                                                   'dice2': dice2, 
                                                                   'dice3': dice3, 
-                                                                  "dice_loss1":dice_loss1})
+                                                                  "dice_loss":dice_loss})
         grad_train[i] = get_grad(h[1], h[2], model)
-        #print(f"{grad_train[i]}")
+        
 
-    np.savez(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/test_totdice/gradtrain_{h[0][0]}.npz", grad_test = grad_train)
+    np.savez(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/test_totdice/gradtrain_{h[0][0]}.npz", grad_train = grad_train)
     print(f"gradtrain_{h[0][0]}.npz")
     print("\t")
     
