8441da382f787b0b31423919c37fbbb405273820
Tommaso Torda <tommaso.torda@gmail.com>
2022-11-24 16:55:08 +0000

    Update TracIn.py

--- TracIn.py
+++ TracIn.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
 os.environ["CUDA_VISIBLE_DEVICES"]="2"
 from nibabel.testing import data_path
 import nibabel as nib
@@ -18,27 +12,18 @@ import time
 import datetime
 import json
 
-
-# In[2]:
-
-
 import itertools
 from keras.models import load_model
 from keras.utils.vis_utils import plot_model
 from sklearn import preprocessing
 
 
-# In[3]:
+search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/train_dice1/" #In this example we evaluate tracin only for label 1
 
-
-search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/train_dice1/"
 os.chdir(search_dir)
 file_list_gradtrain = filter(os.path.isfile, os.listdir(search_dir))
 file_list_gradtrain = [os.path.join(search_dir, f) for f in file_list_gradtrain] # add path to each file
 file_list_gradtrain.sort(key=lambda x: os.path.getmtime(x))
-
-
-# In[4]:
 
 
 search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/gradients/test_dice1/"
@@ -48,17 +33,11 @@ file_list_gradtest = [os.path.join(search_dir, f) for f in file_list_gradtest] #
 file_list_gradtest.sort(key=lambda x: os.path.getmtime(x))
 
 
-# In[5]:
-
-
 search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/DataTracin/"
 os.chdir(search_dir)
 file_list_train = filter(os.path.isfile, os.listdir(search_dir))
 file_list_train = [os.path.join(search_dir, f) for f in file_list_train] # add path to each file
 file_list_train.sort(key=lambda x: os.path.getmtime(x))
-
-
-# In[6]:
 
 
 search_dir = "/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/DataTracin_test/"
@@ -68,13 +47,7 @@ file_list_test = [os.path.join(search_dir, f) for f in file_list_test] # add pat
 file_list_test.sort(key=lambda x: os.path.getmtime(x))
 
 
-# In[7]:
-
-
 file = glob(os.path.join('/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/DataTracin/', "*"))
-
-
-# In[8]:
 
 
 def load_grad_train(file):
@@ -86,9 +59,6 @@ def load_grad_test(file):
     data = np.load(file, allow_pickle=True)
     index = int(file[83:len(file)-4])
     return index, data['grad_test']
-
-
-# In[9]:
 
 
 def load_image_train(image_file):
@@ -111,9 +81,7 @@ def test_reshape(index, X,Y):
 
 BATCH_SIZE = 1
 
-
-# In[10]:
-
+#load all the data
 
 train_grad = tf.data.Dataset.list_files(file_list_gradtrain, shuffle = False)
 train_grad = train_grad.map(lambda item: tf.numpy_function(
@@ -121,16 +89,10 @@ train_grad = train_grad.map(lambda item: tf.numpy_function(
           num_parallel_calls=tf.data.AUTOTUNE)
 
 
-# In[11]:
-
-
 test_grad = tf.data.Dataset.list_files(file_list_gradtest, shuffle = False)
 test_grad = test_grad.map(lambda item: tf.numpy_function(
           load_grad_test, [item], [tf.int64, tf.float32]),
           num_parallel_calls=tf.data.AUTOTUNE)
-
-
-# In[12]:
 
 
 train_dataset = tf.data.Dataset.list_files(file_list_train, shuffle = False)
@@ -141,9 +103,6 @@ train_dataset = train_dataset.map(test_reshape)
 train_dataset = train_dataset.batch(BATCH_SIZE)
 
 
-# In[13]:
-
-
 test_dataset = tf.data.Dataset.list_files(file_list_test, shuffle = False)
 test_dataset = test_dataset.map(lambda item: tf.numpy_function(
           load_image_test, [item], [tf.int64, tf.double, tf.double]),
@@ -151,10 +110,7 @@ test_dataset = test_dataset.map(lambda item: tf.numpy_function(
 test_dataset = test_dataset.map(test_reshape)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
-
-# In[14]:
-
-
+#track the order of the example
 map_test = {}
 map_train = {}
 
@@ -165,9 +121,6 @@ for i,j in enumerate(test_dataset):
     
 
 
-# In[15]:
-
-
 def tracin(grad1,grad2):
     s = grad1@grad2.transpose()
     s = np.array(s, dtype = float)
@@ -175,10 +128,7 @@ def tracin(grad1,grad2):
     return s
 
 
-# In[16]:
-
-
-def my_gen(train_grad, test_grad):
+def my_function(train_grad, test_grad):
     
     score = {}
     
@@ -190,22 +140,16 @@ def my_gen(train_grad, test_grad):
     
 
 
-# In[17]:
-
-
 final_score = {}
 for i,j in enumerate(test_grad):
-        final_score[int(j[0])] = my_gen(train_grad, j[1])
+        final_score[int(j[0])] = my_function(train_grad, j[1])
         print(f"test_{j[0]}")
 
-
-# In[18]:
-
-
-b = {}
+tracin_score = {}
+#Regarding the fact that we work in 2D, in this case, proponent and opponent are not the specific patients, but one brain slice of the patients. If we want to have as prop and opp the specific patient, we must compare the comulative Tracin score for all the slices of their brain. 
 
 for k in range(len(final_score)):
-    print(f"test_{k}")
+    print(f"test_{k}") #we select a test example
     c = {}
     for j in range(0, len(final_score[k]), 10):
         m = 0 
@@ -213,75 +157,40 @@ for k in range(len(final_score)):
         index = j
         
         for i in range(j,j+10):
-            a += final_score[k][i]
+            a += final_score[k][i] #we sum all the slice of a training patient
         
             if abs(final_score[k][i]) > m:
-                m = abs(final_score[k][i])
+                m = abs(final_score[k][i]) #we keep only the slice of the training patients with maximum tracin score. This help for plotting the results
                 index = i
                 
         print(f"max : {m}, index :{index}")
         c[index] = a
     print("\t")
-    b[k] = c 
+    tracin_score[k] = c 
 
 
-# In[19]:
+np.save("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/score_dice1.npy", tracin_score) 
+
+tracin_score = np.load("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/score_dice1.npy",allow_pickle='TRUE').item()
+
+#we sort the tracin score and took the first, and last 20 example as proponents and opponents
+proponents_score = [sorted(b[i].items(), key=lambda x: x[1], reverse = True) for i in range(len(tracin_score))][:20]
+opponents_score = [sorted(b[i].items(), key=lambda x: x[1]) for i in range(len(tracin_score))][:20]
 
 
-np.save("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/score_dice1.npy", b) 
-
-b = np.load("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/score_dice1.npy",allow_pickle='TRUE').item()
-
-
-# In[20]:
-
-
-proponents_score = [sorted(b[i].items(), key=lambda x: x[1], reverse = True) for i in range(len(b))]
-opponents_score = [sorted(b[i].items(), key=lambda x: x[1]) for i in range(len(b))]
-
-
-# In[22]:
-
-
-np.savez(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/prop_index_dice1.npz", proponents = proponents_index)
-
-
-# In[21]:
-
-
+#extract the index of the examples
 proponents_index = np.zeros((len(proponents_score), 207))
 for i,j in enumerate(proponents_score):
     for k,h in enumerate(proponents_score[i]):
         proponents_index[i][k] = int(h[0])
-
-
-# In[ ]:
-
 
 opponents_index = np.zeros((len(opponents_score), 207))
 for i,j in enumerate(opponents_score):
     for k,h in enumerate(opponents_score[i]):
         opponents_index[i][k] = int(h[0])
 
-
-# In[ ]:
-
-
-for i in range(0, len(proponents_index), 10):
-    print(f"index : {int(i)}, {proponents_index[int(i)]}")
-    if i == 210: break
-
-
-# In[ ]:
-
-
-for i in range(0, len(opponents_index), 10):
-    print(f"index : {int(i)}, {opponents_index[int(i)]}")
-    if i == 210: break
-
-
-# In[ ]:
-
+np.savez(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/prop_index_dice1.npz", proponents = proponents_index)
+np.savez(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Radiomic_Features/opp_index_dice1.npz", opponents = opponents_index)
 
 def dice0(y_true, y_pred, smooth = 1e-7):
     y_true_f = tf.reshape(tf.cast(y_true[:,:,:,0], 'float32'), [len(y_true), -1]) 
@@ -306,7 +215,7 @@ def dice3(y_true, y_pred, smooth = 1e-7):
     return (2*tf.reduce_sum(tf.abs(y_true_f*y_pred_f), axis = 1))/(tf.reduce_sum(
         y_true_f**2 + y_pred_f**2, axis = 1)+smooth)
 
-def dice_loss1(y_true, y_pred):
+def dice_loss(y_true, y_pred):
     a0 = 0
     a1 = 1
     a2 = 1
@@ -315,7 +224,6 @@ def dice_loss1(y_true, y_pred):
         y_true,y_pred)+a3*dice3(y_true,y_pred))/(a0+a1+a2+a3)
 
 
-# In[ ]:
 
 
 checkpoint_dir = '/home/tordatom/Dati_Imaging/BraTs_19/checkpoints/training_checkpoints_Unet'
@@ -323,39 +231,25 @@ file_list_ckpt = glob(os.path.join(checkpoint_dir, "*"))
 file_list_ckpt.sort()
 
 
-# In[ ]:
 
 
 model = tf.keras.models.load_model(file_list_ckpt[-1], custom_objects={'dice0': dice0, 
                                                           'dice1': dice1,
                                                           'dice2': dice2, 
                                                           'dice3': dice3, 
-                                                          "dice_loss1":dice_loss1})
+                                                          "dice_loss":dice_loss})
 
 
-# In[ ]:
 
 
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images")
-
-
-# In[ ]:
-
-
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/totdice")
-
-
-# In[ ]:
-
-
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/test")
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/dice1")
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/dice2")
 os.mkdir("/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/dice3")
 
-
-# In[ ]:
-
+#we plot the opponents and proponents for a given test example, choosing the label that we want to monitoring.
 
 for i in range(70,len(proponents_index), 10):
     print(f"proponents_for_test_{i}")
@@ -424,76 +318,9 @@ for i in range(70,len(opponents_index),10):
     if i == 110: break
 
 
-# In[ ]:
 
 
-proponents_index = [1410,633,774,1955,356]
-opponents_index = [1949,1735,1543,1795,1728]
 
-
-# In[ ]:
-
-
-for index in proponents_index:
-    index = int(index)
-    channel = next(itertools.islice(train_dataset, map_train[index], map_train[index]+1))[1]
-    seg = next(itertools.islice(train_dataset, map_train[index], map_train[index]+1))[2]
-    plt.figure(figsize=(12, 8))
-    plt.subplot(131)
-    plt.title(f"Channel: 3, index: {index}")
-    plt.imshow(channel[0,:,:,3])
-    plt.subplot(132)
-    plt.title("Segmentation")
-    plt.imshow(seg[0,:,:,3])
-    plt.subplot(133)
-    plt.title("Pred Segmentation")
-    plt.imshow(model.predict(channel)[0,:,:,3])
-    plt.savefig(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/dice3/proponents_of_test_70/dice3_train_proponent_{index}")
-    plt.show()
-
-
-# In[ ]:
-
-
-for i in range(70,110, 10):
-    channel = next(itertools.islice(test_dataset, map_test[i], map_test[i]+1))[1]
-    seg = next(itertools.islice(test_dataset, map_test[i], map_test[i]+1))[2]
-    plt.figure(figsize=(12, 8))
-    plt.subplot(131)
-    plt.title(f"Channel 3, index: {i}")
-    plt.imshow(channel[0,:,:,3])
-    plt.subplot(132)
-    plt.title("Segmentation")
-    plt.imshow(seg[0,:,:,3])
-    plt.subplot(133)
-    plt.title("Pred Segmentation")
-    plt.imshow(model.predict(channel)[0,:,:,3])
-    plt.savefig(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/test/dice3_test_example_{i}")
-    plt.show()
-
-
-# In[ ]:
-
-
-for index in opponents_index: 
-    index = int(index)
-    channel = next(itertools.islice(train_dataset, map_train[index], map_train[index]+1))[1]
-    seg = next(itertools.islice(train_dataset, map_train[index], map_train[index]+1))[2]
-    plt.figure(figsize=(12, 8))
-    plt.subplot(131)
-    plt.title(f"Channel: 3, index: {index}")
-    plt.imshow(channel[0,:,:,3])
-    plt.subplot(132)
-    plt.title("Segmentation")
-    plt.imshow(seg[0,:,:,3])
-    plt.subplot(133)
-    plt.title("Pred Segmentation")
-    plt.imshow(model.predict(channel)[0,:,:,3])
-    plt.savefig(f"/home/tordatom/Dati_Imaging/BraTs_19/Segmentation2D/Tracin_Images/dice3/opponents_of_test_100/dice3_train_opponents_{index}")
-    plt.show()
-
-
-# In[ ]:
 
 
 
